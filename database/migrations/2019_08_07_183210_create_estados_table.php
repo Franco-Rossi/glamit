a1c1fb0d562a26a69d1c84370774d0f5d6b40a1c
FrancoRossi <francorossipriv@gmail.com>
2019-08-07 16:26:46 -0300

    migrations

--- database/migrations/2019_08_07_183210_create_estados_table.php
+++ database/migrations/2019_08_07_183210_create_estados_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class CreateEstadosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,9 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('estados', function (Blueprint $table) {
+            $table->string('estado', 32)->nullable(false)->primary();
+            $table->string('subestado', 32)->nullable(false);
         });
     }
 
@@ -27,6 +26,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('estados');
     }
 }
---
+++ database/migrations/2019_08_07_183221_create_maestro_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMaestroTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('maestro', function (Blueprint $table) {
+            $table->bigIncrements('nro_pedido');/* Utilizado como key autoincremental */
+            $table->string('metodo_envio')->nullable();
+            $table->string('metodo_pago')->nullable();
+            $table->timestamp('fecha_creacion')->nullable();
+            $table->timestamp('fecha_actualizacion')->nullable(false)->useCurrent();
+            $table->timestamp('fecha_factura')->nullable();
+            $table->string('estado', 32)->nullable();
+            $table->string('subestado', 32)->nullable();
+            $table->string('tienda')->nullable();
+            $table->decimal('costo_envio', 12, 4)->nullable();
+            $table->decimal('intereses', 12, 4)->nullable();
+            $table->decimal('total', 12, 4)->nullable();
+            $table->string('cliente_nombre')->nullable();
+            $table->string('cliente_email')->nullable();
+            $table->string('cliente_telefono')->nullable();
+            $table->string('cliente_direccion')->nullable();
+            $table->string('item_nombre')->nullable();
+            $table->string('item_codigo')->nullable();
+            $table->decimal('item_preciounitario', 12, 4)->nullable();
+            $table->integer('item_cantidad')->nullable();
+            $table->integer('item_subtotal')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('maestro');
+    }
+}
